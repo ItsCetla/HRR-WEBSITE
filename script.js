cb024a931c96b664bf209553878d3a43d2d4da85
ItsCetla <47283326+ItsCetla@users.noreply.github.com>
2024-02-23 21:33:07 -0500

    V 0.01

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.querySelector('.feature button').addEventListener('click', function() {
-    console.log('Learn More about our Featured Product!');
-});
+function formatDate(date) {
+    var today = new Date();
+    var day = ('0' + today.getDate()).slice(-2); // Add leading zero if necessary
+    var month = ('0' + (today.getMonth() + 1)).slice(-2); // Add leading zero if necessary, month is 0-indexed
+    var year = today.getFullYear().toString().substr(-2); // Get the last two digits of the year
+
+    return month + '/' + day + '/' + year; // returns MM/DD/YY format
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var currentDate = new Date();
+    document.getElementById('date').textContent = formatDate(currentDate);
+  });
